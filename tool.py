8653156d02ffa39b6ffda0b0c310c33f33492603
wanghao15536870732 <1484290617@qq.com>
2021-01-24 16:50:11 +0800

    add photos

--- tool.py
+++ tool.py
@@ -166,7 +166,7 @@ def git_operation():
     ----------
     需要安装git命令行工具，并且添加到环境变量中
     '''
-    os.system('git add --all')
+    os.system('git add .')
     os.system('git commit -m "add photos"')
     os.system('git push origin master')
 
